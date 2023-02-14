1020589109b66d207a5c07e523367c936e79fa6a
Sakchyam-Nakarmi <nsakchyam21@tbc.edu.np>
2023-02-14 18:12:18 +0545

    Extra exercise for dice roller completed

--- app/src/main/java/com/example/lab_1diceroller/MainActivity.kt
+++ app/src/main/java/com/example/lab_1diceroller/MainActivity.kt
@@ -10,12 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val btn:Button = findViewById(R.id.button)
-        val txt:TextView = findViewById(R.id.textView)
+        val btn :Button = findViewById(R.id.button)
+        val txt1:TextView = findViewById(R.id.textView)
+        val txt2:TextView = findViewById(R.id.textView2)
         btn.setOnClickListener{
             val diceNum = (1..6).random()
-            txt.text= "" + diceNum
+            txt1.text= "" + diceNum
+            val diceNum2 = (1..6).random()
+            txt2.text = ""+diceNum2
         }
     }
 }
